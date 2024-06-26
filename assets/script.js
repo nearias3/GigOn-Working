4c0a654b246705043ec5475e526cccb18f3ae669
nearias3 <127260408+nearias3@users.noreply.github.com>
2024-06-26 00:05:09 -0500

    Added more code

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,11 @@ const authorizationEndpoint = "https://accounts.spotify.com/authorize";
 const tokenEndpoint = "https://accounts.spotify.com/api/token";
 const scope = "user-read-private user-read-email";
 
+// Spotify login button
+document
+  .getElementById("spotifyAuthenticationBtn")
+  .addEventListener("click", loginWithSpotifyClick);
+
 // Data structure that manages the current active token, caching it in localStorage
 const currentToken = {
   get access_token() {
@@ -14,7 +19,7 @@ const currentToken = {
     return localStorage.getItem("refresh_token") || null;
   },
   get expires_in() {
-    return localStorage.getItem("refresh_in") || null;
+    return localStorage.getItem("expires_in") || null;
   },
   get expires() {
     return localStorage.getItem("expires") || null;
@@ -38,35 +43,39 @@ const code = args.get("code");
 
 // If we find a code, we're in a callback, do a token exchange
 if (code) {
-  const token = await getToken(code);
-  currentToken.save(token);
+  (async () => {
+    const token = await getToken(code);
+    currentToken.save(token);
 
-  // Remove code from URL so we can refresh correctly.
-  const url = new URL(window.location.href);
-  url.searchParams.delete("code");
+    // Remove code from URL so we can refresh correctly.
+    const url = new URL(window.location.href);
+    url.searchParams.delete("code");
 
-  const updatedUrl = url.search ? url.href : url.href.replace("?", "");
-  window.history.replaceState({}, document.title, updatedUrl);
+    const updatedUrl = url.search ? url.href : url.href.replace("?", "");
+    window.history.replaceState({}, document.title, updatedUrl);
+  })();
 }
 
 // If we have a token, we're logged in, so fetch user data and render logged in template
 if (currentToken.access_token) {
-  const userData = await getUserData();
-  renderTemplate("main", "logged-in-template", userData);
-  renderTemplate("oauth", "oauth-template", currentToken);
-}
-
-// Otherwise we're not logged in, so render the login template
-if (!currentToken.access_token) {
-  renderTemplate("main", "login");
+  (async () => {
+    const userData = await getUserData();
+    renderTemplate("mainContent", "logged-in-template", userData);
+    renderTemplate("oauth", "oauth-template", currentToken);
+  })();
+} else {
+  // Otherwise we're not logged in, so render the login template
+  renderTemplate("mainContent", "login");
 }
 
 async function redirectToSpotifyAuthorize() {
+  console.log("Redirecting to Spotify for authorization...");
+
   const possible =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   const randomValues = crypto.getRandomValues(new Uint8Array(64));
   const randomString = randomValues.reduce(
-    (acc, x) => acc + possible[x % possible.length],
+    (acc, x) => acc + possible.charAt(x % possible.length),
     ""
   );
 
@@ -94,10 +103,11 @@ async function redirectToSpotifyAuthorize() {
   };
 
   authUrl.search = new URLSearchParams(params).toString();
+  console.log(`Auth URL: ${authUrl.toString()}`);
   window.location.href = authUrl.toString(); // Redirect the user to the authorization server for login
 }
 
-// Soptify API Calls
+// Spotify API Calls
 async function getToken(code) {
   const code_verifier = localStorage.getItem("code_verifier");
 
@@ -144,6 +154,7 @@ async function getUserData() {
 }
 
 // Click handlers
+
 async function loginWithSpotifyClick() {
   await redirectToSpotifyAuthorize();
 }
